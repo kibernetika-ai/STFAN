2785da19aef4673e58c138c67c57b50dfa82e227
Nikolay Mahotkin <nikolay.makhotkin@gmail.com>
2020-05-14 14:32:33 +0400

    CPU image cannot be built

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-docker build --tag kuberlab/stfan:cpu -f Dockerfile .
-docker build --tag kuberlab/stfan:gpu -f Dockerfile.gpu .
+docker build --tag kuberlab/stfan:gpu -f Dockerfile .
 
 if [ "$1" == "--push" ];
 then
-    docker push kuberlab/stfan:cpu
     docker push kuberlab/stfan:gpu
 fi
